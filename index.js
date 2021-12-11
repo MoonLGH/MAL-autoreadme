55bc8c8c2b60720d6146513ac845b643941ee85b
MoonLGH <moonlmc1212@gmail.com>
2021-12-11 13:02:53 +0700

    not updating if no updates

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ async function init(){
         throw new Error("Enter a token");
     }
 
+    if(readme === newreadme){
+        console.log("No changes")
+        return
+    }
     console.log("Pushing to Github")
     let file = git.createKoreFile({
         adaptor: git.createGitHubAdaptor({
